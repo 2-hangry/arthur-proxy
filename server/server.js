f0c5c52b55daaf49c3954c37184ed8030b48fe19
Arthur Tharp <atharp@uci.edu>
2018-09-23 18:35:25 -0700

    server endpoints modified

--- server/server.js
+++ server/server.js
@@ -15,18 +15,29 @@ router.all('/:id/restaurantInfo*', (req, res) => {
 });
 
 router.all('/:id/reviews*', (req, res) => {
-  res.redirect(307, `http://52.55.139.226:80${req.url}`);
+  // res.redirect(307, `http://52.55.139.226:80${req.url}`);
+  res.redirect(307, `http://18.206.196.154:80/${req.params.id}`);
 });
 
 router.all('/:id/images*', (req, res) => {
-  res.redirect(307, `http://18.188.153.185:80${req.url}`);
+  // res.redirect(307, `http://18.188.153.185:80${req.url}`);
+  res.redirect(307, `http://18.224.24.70:80${req.url}`);
 });
 
 app.get('/businesses/:id/restaurantInfo', function(req, res) {
-  // res.redirect(`http://127.0.0.1:3002/restaurantInfo-service/${req.params.id}`);
   res.redirect(
     `http://35.183.134.103:80/restaurantInfo-service/${req.params.id}`,
   );
+});
+
+app.get('/businesses/:id/images', function(req, res) {
+  // res.redirect(`http://18.188.153.185:80/images-service/${req.params.id}`);
+  res.redirect(`http://18.224.24.70:80/images-service/${req.params.id}`);
+});
+
+app.get('/businesses/:id/reviews', function(req, res) {
+  // res.redirect(`http://52.55.139.226:80/reviews-service/${req.params.id}`);
+  res.redirect(`http://18.206.196.154:80/reviews-service/${req.params.id}`);
 });
 
 app.listen(port, () => {
